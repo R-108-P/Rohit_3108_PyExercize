4f2b8c8371f8bc11bda35ee590079ef5614fbfbf
R-108-P <102592257+R-108-P@users.noreply.github.com>
2022-07-27 01:35:31 +0200

    Add files via upload

---
+++ Nearby_line.py
@@ -0,0 +1,31 @@
+from shapely.geometry import Point
+import math
+
+pt = (0, 0)
+angle = 0
+lines = [((1, -1), (1, 1)), ((2, -1), (2, 1))]
+data = []
+dct = {}
+
+def origin_lineseg_dist(pt1, pt2) -> tuple[list, float] | None:
+    p = pt2[1] - pt1[1]
+    q = pt2[0] - pt1[0]
+
+    if (p == 0 and angle == 0) or (angle == 90 and q == 0):
+        return
+    elif q != 0:
+        m = p / q  # y = mx + c , m is slope
+
+        r = pt1[1] - (m * pt1[0])
+        m_start_pt = math.tan(math.radians(angle))
+        x_coord = round(r / (m_start_pt - m), 3)
+        y_coord = round(m * x_coord + r, 3)
+
+    else:
+        x_coord = pt1[0]  # x1
+        m_start_pt = (math.tan(math.radians(angle)))
+        y_coord = m_start_pt * x_coord
+
+    if min(pt1[0], pt2[0]) <= x_coord <= max(pt1[0], pt2[0]) and min(pt1[1], pt2[1]) <= \
+            y_coord <= max(pt1[1], pt2[1]):
+        return [pt1, pt2], round(math.dist((0, 0), (x_coord, y_coord)))
